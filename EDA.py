7ef26721d6c12223ac6d6f34851f21a3002269e9
Nicholas Ogari <ogarinicholas@gmail.com>
2025-10-12 23:13:11 +0300

    .

--- EDA.py
+++ EDA.py
@@ -5,3 +5,10 @@ dataset.head(10)
 dulpicated_rows = dataset[dataset.duplicated()]
 print("Duplicated rows:")
 print(dulpicated_rows)
+
+AgeDuplicate=dataset[dataset['Age'].duplicated()]
+print("Duplicated Age rows:")
+print(AgeDuplicate)
+
+print("\nUnique values in 'AGE' Column:")
+print(dataset['Age'].value_counts())
